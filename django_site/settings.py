c5d047a83d8cd92c39f5fa3a6bfed84f0adc0384
NesKapone <84278828+NesKapone@users.noreply.github.com>
2021-08-25 17:19:47 +0300

    Update settings.py

--- django_site/settings.py
+++ django_site/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import dj_database_url
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+DISABLE_COLLECTSTATIC=1
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
